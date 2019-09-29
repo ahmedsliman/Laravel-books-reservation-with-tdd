89df56184417c10dab4b1394843b23f8dc4b9081
ahmed soliman <ahmedsliman@gmail.com>
2019-09-29 21:11:43 +0200

    Tailwind && React

--- app/Http/Controllers/AuthorsController.php
+++ app/Http/Controllers/AuthorsController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class AuthorsController extends Controller
 {
+
+    public function create()
+    {
+        return view('authors.create');
+    }
+
     public function store()
     {
         Author::create($this->validateRequest());
---
+++ resources/views/authors/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="bg-grey-300 h-screen">
+    <div></div>
+</div>
+@endsection
